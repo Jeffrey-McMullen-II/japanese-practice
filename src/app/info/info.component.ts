f13bea4a47e078fc63b8e4842bbaa4971ed70bad
Jeffrey <jeffrey.mcmullen@yahoo.com>
2021-03-07 22:12:04 -0500

    Redirect from landing page to hiragana characters

--- src/app/info/info.component.ts
+++ src/app/info/info.component.ts
@@ -3,6 +3,6 @@ import { Vue } from 'vue-class-component';
 export default class Info extends Vue {
     
     mounted() {
-        this.$router.push('hiragana-characters');
+        this.$router.replace('hiragana-characters');
     }
 }
