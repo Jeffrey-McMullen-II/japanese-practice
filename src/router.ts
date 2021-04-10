5d9354734409375695e57d58b65e75ad26f5f969
Jeffrey <jeffrey.mcmullen@yahoo.com>
2021-04-10 12:56:09 -0400

    Establish routes directly in router creation

--- src/router.ts
+++ src/router.ts
@@ -3,15 +3,13 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import Japanese from './app/japanese/japanese.component.vue';
 import Info from './app/info/info.component.vue';
 
-const routes: Array<RouteRecordRaw> = [
-  { path: '/', name: 'info', component: Info },
-  { path: '/hiragana-characters', name: 'hiragana-characters', component: Japanese },
-  { path: '/hiragana-words', name: 'hiragana-words', component: Japanese },
-  { path: '/katakana-characters', name: 'katakana-characters', component: Japanese },
-  { path: '/katakana-words', name: 'katakana-words', component: Japanese }
-];
-
 export default createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes: [
+    { path: '/', name: 'info', component: Info },
+    { path: '/hiragana-characters', name: 'hiragana-characters', component: Japanese },
+    { path: '/hiragana-words', name: 'hiragana-words', component: Japanese },
+    { path: '/katakana-characters', name: 'katakana-characters', component: Japanese },
+    { path: '/katakana-words', name: 'katakana-words', component: Japanese }
+  ]
 });
